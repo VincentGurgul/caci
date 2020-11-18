a3eef4077600c7bcb0df8cf222b468b594637fa9
Vincent Gurgul <VincentGurgul@Vincents-MacBook-Air-2.local>
2020-11-18 21:27:39 +0100

    added new graph

--- SWP_01/CACI_SWP1_JL.R
+++ SWP_01/CACI_SWP1_JL.R
@@ -12,7 +12,7 @@ data = read.csv("QuestionaireData_CityTrips_csv.csv", stringsAsFactors = FALSE)
 lowbudget=subset.data.frame(data,Avg_Budget==1)
 
 
-#Eindeutige Stadtbesuche über alle Budgetklassen#
+#Eindeutige Stadtbesuche ?ber alle Budgetklassen#
 
 totalvisits=sum(data[,4:23])
 uniquepervisits=(totalvisits/ncol(data))
@@ -26,7 +26,7 @@ uniquepervisitslb=(totalvisitslb/ncol(lowbudget))
 uniquepervisitslb
 
 
-#Beusche Pro Jahr über alle Budgetklassen hinweg#
+#Beusche Pro Jahr ?ber alle Budgetklassen hinweg#
 
 overallvisits=sum(data[,35])
 overallpervisits=(overallvisits/ncol(data))
@@ -34,9 +34,36 @@ overallpervisitsweight=(overallpervisits*2.8)
 overallpervisitsweight
 
 
-#Besuche pro Jahr für Budgetklasse 1#
+#Besuche pro Jahr f?r Budgetklasse 1#
 
 overallvisitslb=sum(lowbudget[,35])
 overallpervisitslb=(overallvisitslb/ncol(lowbudget))
 overallpervisitslbweight=overallpervisitslb*2.8
 overallpervisitslbweight
+
+
+#####
+
+
+dd = data.frame("unique cities visited" = c(uniquepervisitslb, uniquepervisits),
+                "overall cities visited" = c(overallpervisitslbweight, overallpervisitsweight),
+                row.names = c("lowbudget travellers", "all travellers")
+                )
+
+gg = data.frame(x = c("all city visits", "   ", "unique city visits", "  "),
+                y = c(overallpervisitsweight, overallpervisitslbweight, uniquepervisits, uniquepervisitslb),
+                col = c("pink", "skyblue", "pink", "skyblue"))
+
+gg$x = factor(gg$x, levels = gg$x)
+ggplot(data = gg, aes(x,y, fill = col)) + geom_col(color = "black") +
+  theme_minimal() +
+  labs(title = "Comparing City Visits of All Travellers and Lowbudget Travellers", 
+       y = "cities visited", fill = "Type of traveller") +
+  theme(text = element_text(size=10),
+        plot.title = element_text(size = 11, hjust = 3.5), 
+        axis.text.x = element_text(size = 10, hjust = -0.8, vjust = 4),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank()) +
+  scale_fill_identity(guide = "legend",
+                       labels = c("All travellers", "Lowbudget travellers", "All travellers", "Lowbudget travellers"))
+       
